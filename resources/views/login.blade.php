0535656c00097f58a328633a80b2e0e5d25532ce
asif <asiftanvir2006@gmail.com>
2025-05-14 18:00:17 +0530

    Made changes

---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- The order in which link rel is defined for both is very important. -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Asif-Tanvir-2006/connect2k25@main/public/css/login.css">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="view">
+        <div class="padder">
+
+        </div>
+        <div class="box">
+            <div class="wrapper">
+                <div class="caption">Username</div>
+                <div class="upper">
+                    <input type="text" name="" id="">
+                </div>
+                <div class="caption">Password</div>
+                <div class="lower">
+                    <input type="text" name="" id="">
+                </div>
+                <button>Sign In</button>
+                <div class="cap">Not Registered Yet? <a href=""> Sign Up</a></div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
